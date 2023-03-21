36b3d5fa3154cc8ce9583d7c1bcd1c2b215c1d97
consulty <Consult@github07>
2023-03-21 21:32:13 +0530

    first commit

---
+++ client/src/service/api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+// const URL = 'http://localhost:8000';
+const URL='https://inshortsclone.onrender.com';
+
+
+export const getNews = async (page, size = 5) => {
+    try {
+        return await axios.get(`${URL}/news?page=${page}&size=${size}`);
+    } catch (error) {
+        console.log('error while calling getNews API');
+    }
+}
